d13d3227bfb8400238effc55ea40de0d723bb139
Can <hcdayan@hotmail.com>
2022-11-02 12:17:23 +0100

    Initial Commit

---
+++ GymBode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GymBode
+//
+//  Created by Can on 2.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
