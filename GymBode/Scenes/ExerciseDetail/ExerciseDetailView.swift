2275b22ad7a28a9783baa9582549ccd1530bae73
Can <hcdayan@hotmail.com>
2022-11-02 23:39:01 +0100

    Setup mock detail view.

--- GymBode/Scenes/ExerciseDetail/ExerciseDetailView.swift
+++ GymBode/Scenes/ExerciseDetail/ExerciseDetailView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct ExerciseDetailView: View {
+    private let columns = [GridItem(.flexible()), GridItem(.flexible())]
+    
+    //TODO: - Feed viewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .center,
+                   spacing: 32.0) {
+                SwiftUI.Image(systemName: "figure.gymnastics")
+                    .resizable()
+                    .frame(width: 150.0, height: 150.0)
+                    .font(.largeTitle)
+                    .foregroundColor(.purple)
+                Text("2 Handed me Kettlebell Swing")
+                    .font(.title)
+                
+                LazyVGrid(columns: columns, spacing: 16.0) {
+                    // TODO: - Feed Variations
+                    Text("2 Handed me Kettlebell Swing")
+                    Text("2 Handed me Kettlebell Swing")
+                    Text("2 Handed me Kettlebell Swing")
+                    Text("2 Handed me Kettlebell Swing")
+                    Text("2 Handed me Kettlebell Swing")
+                    Text("2 Handed me Kettlebell Swing")
+                    Text("2 Handed me Kettlebell Swing")
+                }
+            }
+        }.padding()
     }
 }
 
