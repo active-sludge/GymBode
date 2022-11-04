acf1eac9ab6ac91085f4548b362fa654706f9cdf
Can <hcdayan@hotmail.com>
2022-11-04 20:54:30 +0100

    Redefine Exercise Service and protocol.

--- GymBode/Network/ExerciseService/ExerciseService.swift
+++ GymBode/Network/ExerciseService/ExerciseService.swift
@@ -8,82 +8,18 @@
 import Foundation
 import Combine
 
-enum ServiceError: Error {
-    case url(URLError)
-    case urlRequest
-    case decode
-}
-
 protocol ExerciseServicable {
-    func getExerciseList() -> AnyPublisher<ExerciseListResponse, Error>
-    func getExerciseDetail(with id: Int)
+    func getExerciseList() // -> AnyPublisher<ExerciseListResponse, Error>
+    func getExerciseDetail(with id: Int) // -> AnyPublisher<Exercise, Error>
 }
 
-final class ExerciseService {
-    enum Path {
-        case exerciseList
-        case exerciseDetail(id: Int)
-        
-        var pathValue: String {
-            switch self {
-            case .exerciseList:
-                return "exerciseinfo"
-            case .exerciseDetail(id: let id):
-                return "exerciseinfo/\(id)"
-            }
-        }
+final class ExerciseService: ExerciseServicable {
+    
+    func getExerciseList() { // -> AnyPublisher<ExerciseListResponse, Error> {
+        // TODO: - call base service
     }
     
-    private func getUrlRequest(for path: Path) -> URLRequest? {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "wger.de"
-        components.path = "/api/v2/\(path.pathValue)"
-        
-        guard let url = components.url else { return nil }
-        let urlRequest = URLRequest(url: url)
-        return urlRequest
-    }
-}
-
-extension ExerciseService: ExerciseServicable {
-    
-    func getExerciseList() -> AnyPublisher<ExerciseListResponse, Error> {
-        var dataTask: URLSessionDataTask?
-        
-        let onSubscription: (Subscription) -> Void = { _ in dataTask?.resume() }
-        let onCancel: () -> Void = { dataTask?.cancel() }
-        
-        return Future<ExerciseListResponse, Error> { [weak self] promise in
-            guard let urlRequest = self?.getUrlRequest(for: .exerciseList) else {
-                promise(.failure(ServiceError.urlRequest))
-                return
-            }
-
-            dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
-                guard let data = data else {
-                    if let error = error {
-                        promise(.failure(error))
-                    }
-                    return
-                }
-                do {
-                    let exerciseListResponse = try JSONDecoder().decode(ExerciseListResponse.self, from: data)
-                    promise(.success(exerciseListResponse))
-                } catch {
-                    promise(.failure(ServiceError.decode))
-                }
-            }
-        }
-        .handleEvents(receiveSubscription: onSubscription,
-                      receiveCancel: onCancel)
-        .receive(on: DispatchQueue.global())
-        .eraseToAnyPublisher()
-        
-    }
-    
-    func getExerciseDetail(with id: Int) {
-        // TODO: - getExerciseDetail
-        
+    func getExerciseDetail(with id: Int) { // -> AnyPublisher<Exercise, Error> {
+        // TODO: - call base service
     }
 }
